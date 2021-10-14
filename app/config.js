de0e16f7cde14e90226b8fe121a0d32989002cbd
Jeremy <jeremygillies@gmail.com>
2021-10-14 09:35:31 +0100

    typo

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Targetted Flood Warning Service',
+  serviceName: 'Targeted Flood Warning Service',
 
   // Default port that prototype runs on
   port: '3000',
