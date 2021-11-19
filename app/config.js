21fbf8505e1166c50a80c276a25f4da59a3deea6
Jeremy <jeremygillies@gmail.com>
2021-11-19 15:00:36 +0000

    New pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Set up and manage flood warnings for your organisation',
+  serviceName: 'Set up flood warnings for your organisation',
 
   // Default port that prototype runs on
   port: '3000',
